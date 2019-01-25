c33b7fb9f2502f4403397e8ab7fc502f123e13e1
Rezwin Rafeek <rezraf77@gmail.com>
2019-01-25 19:52:42 +0530

    first commit

---
+++ movie_organizer.py
@@ -0,0 +1,9 @@
+#! python3
+
+#A python script that when executed inside a folder containing movies will organize all movies in folders along with subtitles
+
+#Step 1 : list the contents of the directories
+
+#Step 2 : Find all unique movie names without taking into consideration their .srt files
+
+#Step 3 : Create folders for each movie having the same name as 
